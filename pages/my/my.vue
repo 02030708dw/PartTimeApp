9b72504eb43f66a51969a36227469bff94a25290
safe030 <3157247754@qq.com>
2024-01-29 17:59:21 +0800

    新增我的接口

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,16 +1,16 @@
 <template>
 	<view class="NavBar_box"><uni-nav-bar :border="false" :title="$t('my.my')" color="#fff" background-color="#0134e1">
-		<template #right>
-			<!--      <div @click="lanControl" class="lan">{{$t('index.top')}} <uni-icons type="bottom" color="#fff"/></div>-->
-			<div class="icons">
-				<uni-icons color="#fff" type="headphones" />
-				<uni-icons color="#fff" type="chat" />
-			</div>
-		</template>
-	</uni-nav-bar></view>
-	
+			<template #right>
+				<!--      <div @click="lanControl" class="lan">{{$t('index.top')}} <uni-icons type="bottom" color="#fff"/></div>-->
+				<div class="icons">
+					<uni-icons color="#fff" type="headphones" />
+					<uni-icons color="#fff" type="chat" />
+				</div>
+			</template>
+		</uni-nav-bar></view>
+
 	<view class="conent_box">
-		
+
 		<view class="hander_box">
 			<!-- 头像 -->
 			<view class="Avatar_box">
@@ -18,11 +18,11 @@
 			</view>
 			<!-- 昵称 -->
 			<view class="Name_box">
-				Roy Warner
+				<span v-if="UserInfo">{{UserInfo.username}}</span>
 			</view>
 			<!-- ID -->
 			<view class="ID_box">
-				ID:RO00083724
+				ID:<span v-if="UserInfo">{{UserInfo.aiaUserid}}</span>
 			</view>
 		</view>
 		<!-- 收益 -->
@@ -33,7 +33,8 @@
 					金币收益
 				</view>
 				<view class="Number_box">
-					1400.00
+					<span v-if="UserInfo">{{UserInfo.money}}</span>
+					
 				</view>
 			</view>
 			<!-- right -->
@@ -44,28 +45,41 @@
 	</view>
 
 </template>
-<script>
+<script setup>
+	import {
+		ref,
+		onMounted
+	} from 'vue';
+	import {
+		https
+	} from '../../utils/https.js'
 	import UniNavBar from "../../uni_modules/uni-nav-bar/components/uni-nav-bar/uni-nav-bar.vue";
 	import UniIcons from "../../uni_modules/uni-icons/components/uni-icons/uni-icons.vue";
-	export default {
-		name: "my",
-		components: {
-			UniIcons,
-			UniNavBar
-		}
+	onMounted(() => {
+		GetUserInfo()
+	})
+	const UserInfo = ref()
+	const GetUserInfo = () => {
+		https('/materialMember/info', 'get').then(res => {
+			console.log(res.data.resultSet.money)
+			UserInfo.value = res.data.resultSet
+		}).catch(err => {
+			console.log(err)
+		})
 	}
 </script>
 
 
 
 <style scoped lang="scss">
-	.NavBar_box{
+	.NavBar_box {
 		width: 100vw;
 		position: fixed;
 		top: 0rpx;
 		left: 0rpx;
 		z-index: 9999;
 	}
+
 	.conent_box {
 		padding-top: 78rpx;
 		width: 100vw;
@@ -74,8 +88,8 @@
 		align-items: center;
 		flex-direction: column;
 		background-color: #F5F8FF;
-		
-		.hander_box{
+
+		.hander_box {
 			width: 750rpx;
 			height: 496rpx;
 			background: #0256FF;
@@ -85,20 +99,23 @@
 			align-items: center;
 			justify-content: center;
 			flex-direction: column;
+
 			// 头像
-			.Avatar_box{
+			.Avatar_box {
 				width: 172rpx;
 				height: 172rpx;
-				box-shadow: 0rpx 8rpx 20rpx 0rpx rgba(0,68,204,0.5);
+				box-shadow: 0rpx 8rpx 20rpx 0rpx rgba(0, 68, 204, 0.5);
 				border: 2rpx solid #578FFF;
 				border-radius: 50%;
-				image{
+
+				image {
 					border-radius: 50%;
 					width: 100%;
 					height: 100%;
 				}
 			}
-			.Name_box{
+
+			.Name_box {
 				margin-top: 32rpx;
 				height: 32rpx;
 				font-size: 32rpx;
@@ -106,7 +123,8 @@
 				color: #FFFFFF;
 				line-height: 32rpx;
 			}
-			.ID_box{
+
+			.ID_box {
 				margin-top: 20rpx;
 				height: 24rpx;
 				font-size: 24rpx;
@@ -115,7 +133,8 @@
 				line-height: 24rpx;
 			}
 		}
-		.income_box{
+
+		.income_box {
 			width: 686rpx;
 			height: 196rpx;
 			background: #FFFFFF;
@@ -125,17 +144,20 @@
 			display: flex;
 			align-items: center;
 			justify-content: space-between;
-			.left_box{
+
+			.left_box {
 				margin-left: 48rpx;
-				.title_box{
-					
+
+				.title_box {
+
 					height: 24rpx;
 					font-size: 24rpx;
 					font-weight: 400;
 					color: #333333;
 					line-height: 24rpx;
 				}
-				.Number_box{
+
+				.Number_box {
 					margin-top: 32rpx;
 					height: 44rpx;
 					font-size: 44rpx;
@@ -144,12 +166,13 @@
 					line-height: 44rpx;
 				}
 			}
-			.right_box{
+
+			.right_box {
 				margin-right: 32rpx;
 				width: 208rpx;
 				height: 72rpx;
 				background: #0256FF;
-				box-shadow: 0rpx 8rpx 20rpx 0rpx rgba(2,86,255,0.29);
+				box-shadow: 0rpx 8rpx 20rpx 0rpx rgba(2, 86, 255, 0.29);
 				border-radius: 36rpx;
 				text-align: center;
 				line-height: 72rpx;
