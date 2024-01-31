cdfedb53aa0c299e4dd6e59c9dae4a6383a543c8
小丑在唱独角戏 <10097251+xczcdjx@user.noreply.gitee.com>
2024-01-31 17:52:50 +0800

    新增任务界面

---
+++ pages/target/target.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="target">
+
+  </view>
+</template>
+
+<script>
+var wv;
+	export default {
+		data() {
+			return {
+        wvSrc:""
+			}
+		},
+    onLoad(option){
+      const e=this.getOpenerEventChannel()
+      e.on('scriptTarget',d=>{
+        const channel = new WebViewJavascriptChannel(bridge);
+        function bridge(msg) {
+          channel.postMessage(JSON.stringify({'type':'target', 'content': d}));
+        }
+      })
+    },
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.target{
+  height: 100%;
+  width: 100%;
+  position: relative;
+}
+</style>
